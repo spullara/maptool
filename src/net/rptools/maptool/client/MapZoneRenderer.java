b3417313324833e3b6ab1cd7a117596c4c61fb70
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2005-05-10 19:06:36 +0000

    correct class license

--- src/net/rptools/maptool/client/MapZoneRenderer.java
+++ src/net/rptools/maptool/client/MapZoneRenderer.java
@@ -1,11 +1,26 @@
-/*
- * $Id$
- *
- * Copyright (C) 2005, Digital Motorworks LP, a wholly owned subsidiary of ADP.
- * The contents of this file are protected under the copyright laws of the
- * United States of America with all rights reserved. This document is
- * confidential and contains proprietary information. Any unauthorized use or
- * disclosure is expressly prohibited.
+/* The MIT License
+ * 
+ * Copyright (c) 2005 David Rice, Trevor Croft
+ * 
+ * Permission is hereby granted, free of charge, to any person 
+ * obtaining a copy of this software and associated documentation files 
+ * (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, 
+ * publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be 
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
  */
 package net.rptools.maptool.client;
 
