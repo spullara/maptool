c9e77d9d0177d34d015b2a0e5eda5745c9d89114
giliath <giliath@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2005-08-16 18:13:52 +0000

    Fixed the distance calculations to actually use the path.  Eventually when we add barriers this will continue to accurately calculate distance.

--- src/net/rptools/maptool/client/walker/astar/AbstractAStarWalker.java
+++ src/net/rptools/maptool/client/walker/astar/AbstractAStarWalker.java
@@ -33,98 +33,156 @@ import net.rptools.maptool.model.Zone;
 
 public abstract class AbstractAStarWalker extends AbstractZoneWalker {
 
-	public AbstractAStarWalker (Zone zone) {
+	public AbstractAStarWalker(Zone zone) {
 		super(zone);
 	}
-	
+
 	private int distance;
-    private int[][] neighborMap = new int[][] {
-            { -1, -1, 10}, { 0, -1, 5}, { 1, -1, 10},
-            { -1, 0, 5},                { 1, 0, 5},
-            { -1, 1, 10},  { 0, 1, 5}, { 1, 1, 10} };
+
+	private int[][] neighborMap = new int[][] { { -1, -1, 10 }, { 0, -1, 5 },
+			{ 1, -1, 10 }, { -1, 0, 5 }, { 1, 0, 5 }, { -1, 1, 10 },
+			{ 0, 1, 5 }, { 1, 1, 10 } };
 
 	@Override
 	protected List<CellPoint> calculatePath(CellPoint start, CellPoint end) {
 		List<AStarCellPoint> openList = new LinkedList<AStarCellPoint>();
 		List<AStarCellPoint> closedList = new LinkedList<AStarCellPoint>();
-		
+
 		openList.add(new AStarCellPoint(start));
-		
+
 		AStarCellPoint node = null;
-		
+
 		while (openList.size() > 0) {
-			 node = openList.remove(0);
-	            if (node.equals(end)) {
-	                break;
-	            }
+			node = openList.remove(0);
+			if (node.equals(end)) {
+				break;
+			}
 
-	            for (int i = 0; i < neighborMap.length; i++) {
-	                int x = node.x + neighborMap[i][0];
-	                int y = node.y + neighborMap[i][1];
-	                AStarCellPoint neighborNode = new AStarCellPoint(x, y);
-	                if (closedList.contains(neighborNode)) {
-	                    continue;
-	                }
+			for (int i = 0; i < neighborMap.length; i++) {
+				int x = node.x + neighborMap[i][0];
+				int y = node.y + neighborMap[i][1];
+				AStarCellPoint neighborNode = new AStarCellPoint(x, y);
+				if (closedList.contains(neighborNode)) {
+					continue;
+				}
 
-	                neighborNode.parent = node;
-	                neighborNode.gScore = gScore(start, neighborNode);
-	                neighborNode.hScore = hScore(neighborNode, end);
+				neighborNode.parent = node;
+				neighborNode.gScore = gScore(start, neighborNode);
+				neighborNode.hScore = hScore(neighborNode, end);
 
-	                if (openList.contains(neighborNode)) {
-	                	AStarCellPoint oldNode = getNode(openList, neighborNode);
+				if (openList.contains(neighborNode)) {
+					AStarCellPoint oldNode = getNode(openList, neighborNode);
 
-	                    // check if it is cheaper to get here the way that we just came, versus the previous path
-	                    if (neighborNode.gScore < oldNode.gScore) {
-	                        oldNode.gScore = neighborNode.gScore;
-	                        neighborNode = oldNode;
-	                        neighborNode.parent = node;
-	                    }
-	                    continue;
-	                }
+					// check if it is cheaper to get here the way that we just
+					// came, versus the previous path
+					if (neighborNode.gScore < oldNode.gScore) {
+						oldNode.gScore = neighborNode.gScore;
+						neighborNode = oldNode;
+						neighborNode.parent = node;
+					}
+					continue;
+				}
 
-	                pushNode(openList, neighborNode);
-	            }
+				pushNode(openList, neighborNode);
+			}
 
-	            closedList.add(node);
-	            node = null;
+			closedList.add(node);
+			node = null;
 
 		}
-		
-		
-		List<CellPoint> ret = new LinkedList<CellPoint>();
-        while (node != null) {
-            ret.add(node);
-            node = node.parent;
-        }
 
-		int numDiag = Math.min(Math.abs(start.x - end.x), Math.abs(start.y - end.y));
-		int numStrt = Math.max(Math.abs(start.x - end.x), Math.abs(start.y - end.y)) - numDiag;
-		distance = getZone().getFeetPerCell() * (numStrt + numDiag + numDiag / 2);
+		List<CellPoint> ret = new LinkedList<CellPoint>();
+		while (node != null) {
+			ret.add(node);
+			node = node.parent;
+		}
+
+		distance = calculateDistance(ret);
 		return ret;
 	}
-	
+
 	private void pushNode(List<AStarCellPoint> list, AStarCellPoint node) {
-        for (int i = 0; i < list.size(); i++) {
-        	AStarCellPoint listNode = list.get(i);
+		for (int i = 0; i < list.size(); i++) {
+			AStarCellPoint listNode = list.get(i);
 
+			if (listNode.cost() > node.cost()) {
+				list.add(i, node);
+				return;
+			}
+		}
+		list.add(node);
+	}
 
-            if (listNode.cost() > node.cost()) {
-                list.add(i, node);
-                return;
-            }
-        }
-        list.add(node);
-    }
+	private AStarCellPoint getNode(List<AStarCellPoint> list,
+			AStarCellPoint node) {
+		for (AStarCellPoint listNode : list) {
+			if (listNode.equals(node)) {
+				return node;
+			}
+		}
+		return null;
+	}
 
-	private AStarCellPoint getNode(List<AStarCellPoint> list, AStarCellPoint node) {
-        for (AStarCellPoint listNode : list) {
-            if (listNode.equals(node)) { return node; }
-        }
-        return null;
-    }
+	protected int calculateDistance(List<CellPoint> path) {
+		if (path == null || path.size() == 0)
+			return 0;
 
-	
+		int distMethod1;
+
+		// LATER: When we add path barriers we have to remove method 1 completely.
+		// I am leaving it now for double checking since we had such a bad time with
+		// distance measurements.
+		
+		{ // method 1 (used for double check of new algorithm)
+			CellPoint start = path.get(0);
+			CellPoint end = path.get(path.size() - 1);
+
+			int numDiag = Math.min(Math.abs(start.x - end.x), Math.abs(start.y
+					- end.y));
+			int numStrt = Math.max(Math.abs(start.x - end.x), Math.abs(start.y
+					- end.y))
+					- numDiag;
+			distMethod1 = getZone().getFeetPerCell()
+					* (numStrt + numDiag + numDiag / 2);
+		}
+
+		int distMethod2;
+		
+		{
+			int numDiag = 0;
+			int numStrt = 0;
+
+			CellPoint previousPoint = null;
+			for (CellPoint point : path) {
+				if (previousPoint != null) {
+					int change = Math.abs(previousPoint.x - point.x) + Math.abs(previousPoint.y - point.y);
+					
+					switch (change) {
+					case 1:
+						numStrt++;
+						break;
+					case 2:
+						numDiag++;
+						break;
+					default:
+						assert false : String.format("Illegal path, cells are not contiguous change=%d", change);
+						return -1;
+					}
+				}
+
+				previousPoint = point;
+			}
+
+			distMethod2 = getZone().getFeetPerCell()
+			* (numStrt + numDiag + numDiag / 2);
+		}
+		
+		assert distMethod1 == distMethod2 : String.format("Inconsistent distances simple=%d, path based=%d", distMethod1, distMethod2);
+		return distMethod2;
+	}
+
 	protected abstract double gScore(CellPoint p1, CellPoint p2);
+
 	protected abstract double hScore(CellPoint p1, CellPoint p2);
 
 	@Override
