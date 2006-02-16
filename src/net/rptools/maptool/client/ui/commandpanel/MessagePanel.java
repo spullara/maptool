1a1180da7b2728351c4d488ca683f8e92828f033
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2006-02-16 03:59:23 +0000

    minor UI updates

--- src/net/rptools/maptool/client/ui/commandpanel/MessagePanel.java
+++ src/net/rptools/maptool/client/ui/commandpanel/MessagePanel.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.UIManager;
 
 public class MessagePanel extends JPanel {
 
@@ -33,6 +34,7 @@ public class MessagePanel extends JPanel {
 		super.addMouseListener(l);
 		textPane.addMouseListener(l);
 		scrollPane.addMouseListener(l);
+		scrollPane.getVerticalScrollBar().addMouseListener(l);
 	}
 	
 	@Override
@@ -40,6 +42,7 @@ public class MessagePanel extends JPanel {
 		super.removeMouseListener(l);
 		textPane.removeMouseListener(l);
 		scrollPane.removeMouseListener(l);
+		scrollPane.getVerticalScrollBar().removeMouseListener(l);
 	}
 	
 	public void addMessage(final String message) {
@@ -47,9 +50,8 @@ public class MessagePanel extends JPanel {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				messages.append("<div>").append(message).append("</div>");
-				textPane.setText("<html><body>" + messages.toString() + "</body></html>");
+				textPane.setText("<html <style>div {font-family: sans-serif; font-size: 11pt} body {margin: 5px, 5px, 5px, 5px}</style>><body>" + messages.toString() + "</body></html>");
 				textPane.scrollRectToVisible(new Rectangle(0, textPane.getSize().height, 1, 1));
-				System.out.println(new Rectangle(0, textPane.getSize().height, 1, 1));
 			}
 		});
 	}
