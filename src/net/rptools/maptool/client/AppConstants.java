d836879bc00e9978b9261715200977dbcde019a0
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2006-04-18 02:21:52 +0000

    remove unused import

--- src/net/rptools/maptool/client/AppConstants.java
+++ src/net/rptools/maptool/client/AppConstants.java
@@ -28,7 +28,6 @@ import java.awt.Color;
 import java.io.File;
 import java.io.FilenameFilter;
 
-import net.rptools.lib.EnvUtil;
 import net.tsc.servicediscovery.ServiceGroup;
 
 public class AppConstants {
