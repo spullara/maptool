7ba37f2316b043a03ad3ca420491de9c721e9e94
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2006-11-09 05:48:51 +0000

    attempt to kill an exception

--- src/net/rptools/maptool/client/ui/tokenpanel/TokenPanelTreeCellRenderer.java
+++ src/net/rptools/maptool/client/ui/tokenpanel/TokenPanelTreeCellRenderer.java
@@ -34,7 +34,7 @@ public class TokenPanelTreeCellRenderer extends DefaultTreeCellRenderer {
         if (value instanceof Token) {
             Token token = (Token) value;
             
-            int height = getPreferredSize().height != 0 ? getPreferredSize().height : 15;
+            int height = getPreferredSize().height > 0 ? getPreferredSize().height : 15;
             if (image == null || image.getHeight() != height) {
             	image = new BufferedImage(height, height, Transparency.TRANSLUCENT);
             }
