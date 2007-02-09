2e09113316436a473893db0777b14a101d0391b0
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2007-02-09 14:33:17 +0000

    optimizations

--- src/net/rptools/maptool/client/ui/zone/FogUtil.java
+++ src/net/rptools/maptool/client/ui/zone/FogUtil.java
@@ -4,8 +4,14 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.PathIterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import net.rptools.maptool.client.MapTool;
@@ -22,7 +28,7 @@ import net.rptools.maptool.util.GraphicsUtil;
 
 public class FogUtil {
 
-	public static Area calculateVisibility(int x, int y, Area vision, Area topology) {
+	public static Area calculateVisibility2(int x, int y, Area vision, Area topology) {
 
 		vision = new Area(vision);
 		vision.transform(AffineTransform.getTranslateInstance(x, y));
@@ -54,6 +60,7 @@ public class FogUtil {
 		
 		Point lastPoint = null;
 		Point lastOutsidePoint = null;
+		int count = 0;
 		for (PathIterator iter = topology.getPathIterator(null); !iter.isDone(); iter.next()) {
 			
 			int type = iter.currentSegment(coords);
@@ -67,7 +74,7 @@ public class FogUtil {
 			}
 			
 			for (int i = 0; i < coordCount; i++) {
-
+				count ++;
 				Point point = new Point((int)coords[i*2], (int)coords[i*2+1]);
 				Point outsidePoint = GraphicsUtil.getProjectedPoint(origin, point, 100000);
 				
@@ -98,7 +105,7 @@ public class FogUtil {
 			}
 			
 		}
-		
+//		System.out.println(count);
 		// Close the area
 		if (lastPoint != null) {
 			Area blockedArea = createBlockArea(firstPoint, lastPoint, lastOutsidePoint, firstOutsidePoint);
@@ -106,6 +113,134 @@ public class FogUtil {
 		}
 
 		return vision;
+	}
+
+	public static Area calculateVisibility(int x, int y, Area vision, Area topology) {
+
+		vision = new Area(vision);
+		vision.transform(AffineTransform.getTranslateInstance(x, y));
+		
+		// sanity check
+		if (topology.contains(x, y)) {
+			return null;
+		}
+		
+		// Trim back to minimize the amount of work to do 
+		// Strategy: create an inverse of the area of the vision (since none of the points outside the vision matter
+		// and subtract that from the actual topology.  Note that since there is no Area inverse, just make a massive
+		// rectangle
+		topology = new Area(topology);
+		
+		Area outsideArea = new Area(new Rectangle(-10000000, -10000000, 20000000, 200000000));
+		outsideArea.subtract(new Area(vision.getBounds()));
+		topology.subtract(outsideArea);
+		
+		List<AreaPoint> pointList = new ArrayList<AreaPoint>();
+		
+		double[] coords = new double[6];
+
+		Point origin = new Point(x, y);
+		
+		Point firstPoint = null;
+		Point firstOutsidePoint = null;
+		
+		Point lastPoint = null;
+		Point lastOutsidePoint = null;
+		Point originPoint = new Point(x, y);
+		for (PathIterator iter = topology.getPathIterator(null); !iter.isDone(); iter.next()) {
+			
+			int type = iter.currentSegment(coords);
+			int coordCount = 0;
+			switch (type) {
+			case PathIterator.SEG_CLOSE: coordCount = 0; break;
+			case PathIterator.SEG_CUBICTO: coordCount = 3; break;
+			case PathIterator.SEG_LINETO: coordCount = 1; break;
+			case PathIterator.SEG_MOVETO: coordCount = 1;break;
+			case PathIterator.SEG_QUADTO: coordCount = 2;break;
+			}
+			
+			for (int i = 0; i < coordCount; i++) {
+
+				Point point = new Point((int)coords[i*2], (int)coords[i*2+1]);
+				Point outsidePoint = GraphicsUtil.getProjectedPoint(origin, point, 100000);
+				
+				if (firstPoint == null) {
+					firstPoint = point;
+					firstOutsidePoint = outsidePoint;
+				}
+
+				if (lastPoint != null) {
+					if (type != PathIterator.SEG_MOVETO) {
+						Area blockedArea = createBlockArea(lastPoint, point, outsidePoint, lastOutsidePoint);
+						pointList.add(new AreaPoint(lastPoint, point, blockedArea, getDistance(originPoint, point)));
+					} else {
+						// Close the last shape
+						if (lastPoint != null) {
+							
+							Area blockedArea = createBlockArea(firstPoint, lastPoint, lastOutsidePoint, firstOutsidePoint);
+							pointList.add(new AreaPoint(lastPoint, point, blockedArea, getDistance(originPoint, point)));
+//							vision.subtract(blockedArea);
+						}
+						
+						firstPoint = point;
+						firstOutsidePoint = outsidePoint;
+					}
+				}
+
+				lastPoint = point;
+				lastOutsidePoint = outsidePoint;
+			}
+			
+		}
+		
+		// Close the area
+		if (lastPoint != null) {
+			Area blockedArea = createBlockArea(firstPoint, lastPoint, lastOutsidePoint, firstOutsidePoint);
+			pointList.add(new AreaPoint(firstPoint, lastPoint, blockedArea, getDistance(originPoint, lastPoint)));
+		}
+
+		Collections.sort(pointList, new Comparator<AreaPoint>() {
+			public int compare(AreaPoint o1, AreaPoint o2) {
+				
+				return o1.distance < o2.distance ? -1 : o2.distance < o1.distance ? 1 : 0;
+			}
+		});
+		
+		int skip = 0;
+		for (AreaPoint point : pointList) {
+			if (!vision.contains(point.p1) && !vision.contains(point.p2)) {
+				skip ++;
+				continue;
+			}
+			vision.subtract(point.quad);
+		}
+
+		// For simplicity, this catches some of the edge cases
+		vision.subtract(topology);
+
+		System.out.println(pointList.size() + " - " + skip);
+		
+		return vision;
+	}	
+	
+	private static double getDistance(Point p1, Point p2) {
+		double a = p2.x - p1.x;
+		double b = p2.y - p1.y;
+		return Math.abs(Math.sqrt(a+b));
+	}
+	
+	private static class AreaPoint {
+		Point p1;
+		Point p2;
+		Area quad;
+		double distance;
+		
+		public AreaPoint(Point p1, Point p2, Area quad, double distance) {
+			this.p1 = p1;
+			this.p2 = p2;
+			this.quad = quad;
+			this.distance = distance;
+		}
 	}
 	
 	private static Area createBlockArea(Point p1, Point p2, Point p3, Point p4) {
@@ -249,5 +384,35 @@ public class FogUtil {
 			MapTool.serverCommand().exposeFoW(zone.getId(), visionArea);
 		}
 		
+	}
+	
+	public static void main(String[] args) {
+		
+		Area topology = new Area();
+		Random r = new Random(12345);
+		for (int i = 0; i < 200; i++) {
+			int x = r.nextInt(5000);
+			int y = r.nextInt(5000);
+			int w = r.nextInt(25) + 25;
+			int h = r.nextInt(25) + 25;
+			
+			topology.add(new Area(new Ellipse2D.Float(x, y, w, h)));
+		}
+		
+		topology.subtract(new Area(new Ellipse2D.Float(2500-10, 2500-10, 20, 20)));
+		
+		Area vision = new Area(new Ellipse2D.Float(0, 0, 2000, 2000));
+		
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			calculateVisibility(2500, 2500, vision, topology);
+		}
+		System.out.println("1: " + (System.currentTimeMillis() - start));
+		
+		start = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			calculateVisibility2(2500, 2500, vision, topology);
+		}
+		System.out.println("2: " + (System.currentTimeMillis() - start));
 	}
 }
