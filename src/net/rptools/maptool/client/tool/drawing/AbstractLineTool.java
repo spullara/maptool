3b15297829194a1afd41e62ce182a78633e9283b
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2007-02-19 22:47:01 +0000

    tweak for line vs area check

--- src/net/rptools/maptool/client/tool/drawing/AbstractLineTool.java
+++ src/net/rptools/maptool/client/tool/drawing/AbstractLineTool.java
@@ -112,7 +112,7 @@ public abstract class AbstractLineTool extends AbstractDrawingTool implements Mo
         }
         
         Drawable drawable = line;
-        if (isBackgroundFill(e)) {
+        if (isBackgroundFill(e) && line.getPoints().size() > 3) { // TODO: There's a bug where the last point is duplicated, hence 3 points
         	drawable = new ShapeDrawable(getPolygon(line));
         }
 
