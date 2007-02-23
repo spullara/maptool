ec6ba08181ea49a9f7bdd457e747aadeb51099c8
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2007-02-23 04:06:16 +0000

    bug fix, invisible zones

--- src/net/rptools/maptool/client/ui/ZoneSelectionPopup.java
+++ src/net/rptools/maptool/client/ui/ZoneSelectionPopup.java
@@ -4,9 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -89,17 +91,31 @@ public class ZoneSelectionPopup extends JPopupMenu {
 		@Override
 		protected void paintComponent(Graphics g) {
 
+			Graphics2D g2d = (Graphics2D) g;
 			Insets insets = getInsets();
-			
+			Object oldAA = g2d.getRenderingHint(RenderingHints.KEY_ANTIALIASING);
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			// Background
 			Dimension size = getSize();
 			g.setColor(renderer == MapTool.getFrame().getCurrentZoneRenderer() ? Color.lightGray : Color.white);
 			g.fillRect(0, 0, size.width, size.height);
+
+			if (!renderer.getZone().isVisible()) {
+				g.setColor(Color.darkGray);
+				
+				for (int i = 0; i < size.width || i < size.height; i += 10) {
+					g.drawLine(i, 0, 0, i);
+				}
+			}
 			
+			// Label
 			String name = renderer.getZone().getName();
 			if (name == null || name.length() == 0) {
 				name = "Map";
 			}
 
+			// Map
 			BufferedImage img = renderer.getMiniImage(MAP_SIZE_WIDTH);
 	        if (img == null || img == ImageManager.UNKNOWN_IMAGE) {
 	            img = ImageManager.UNKNOWN_IMAGE;
@@ -115,6 +131,8 @@ public class ZoneSelectionPopup extends JPopupMenu {
 			g.setColor(Color.black);
 			int nameWidth = SwingUtilities.computeStringWidth(g.getFontMetrics(), name);
 			g.drawString(name, size.width/2 - nameWidth/2, size.height - g.getFontMetrics().getDescent() - insets.bottom);
+			
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, oldAA);
 		}
 		
 		@Override
