6a8d4100d0f1ffc6f2ac07bc69cdb4683349be0b
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2008-01-28 19:29:15 +0000

    contributed by phergus remove need for "run" parameter

--- src/net/rptools/maptool/client/LaunchInstructions.java
+++ src/net/rptools/maptool/client/LaunchInstructions.java
@@ -29,12 +29,14 @@ import javax.swing.JOptionPane;
 
 public class LaunchInstructions {
 
-	private static final String USAGE = "<html><body>You are running MapTool with minimal memory allocated, you may experience odd behavior, " +
+	private static final String USAGE = "<html><body width=\"400\">You are running MapTool with minimal memory allocated, you may experience odd behavior, " +
 			"especially when connecting to or hosting a server.<br><br>  " +
 			"MapTool will launch anyway, but it is recommended that you use one of the 'Launch' scripts instead.</body></html>";
 	
 	public static void main(String[] args) {
-		if (args.length == 0 || !"run".equals(args[0])) {
+		long mem = Runtime.getRuntime().maxMemory();
+
+		if (mem < 256000000) { // maxMemory() doesn't always report the full allocation.
 			JOptionPane.showMessageDialog(new JFrame(), USAGE, "Usage", JOptionPane.INFORMATION_MESSAGE);
 		}
 		MapTool.main(args);
