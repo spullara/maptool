9d65a801b0ec143deb47f82ac413ef6ec9649242
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2008-02-20 07:21:50 +0000

    phergus patch for memory check

--- src/net/rptools/maptool/client/LaunchInstructions.java
+++ src/net/rptools/maptool/client/LaunchInstructions.java
@@ -29,15 +29,21 @@ import javax.swing.JOptionPane;
 
 public class LaunchInstructions {
 
-	private static final String USAGE = "<html><body width=\"400\">You are running MapTool with minimal memory allocated, you may experience odd behavior, " +
-			"especially when connecting to or hosting a server.<br><br>  " +
+	private static final String USAGE = "<html><body width=\"400\">You are running MapTool with insufficient memory allocated (%dMB).<br><br>" + 
+			"You may experience odd behavior, especially when connecting to or hosting a server.<br><br>  " +
 			"MapTool will launch anyway, but it is recommended that you use one of the 'Launch' scripts instead.</body></html>";
 	
 	public static void main(String[] args) {
 		long mem = Runtime.getRuntime().maxMemory();
-
-		if (mem < 256000000) { // maxMemory() doesn't always report the full allocation.
-			JOptionPane.showMessageDialog(new JFrame(), USAGE, "Usage", JOptionPane.INFORMATION_MESSAGE);
+		String msg = new String(String.format(USAGE, mem/(1024*1024)));
+		
+		/* 
+		 * Asking for 256MB via the -Xmx256M switch doesn't guarantee that the amount 
+		 * maxMemory() reports will be 256MB.  The actual amount seems to vary from PC to PC.  
+		 * 200MB seems to be a safe value for now.  <Phergus>
+		 */
+		if (mem < 200*1024*1024) {
+			JOptionPane.showMessageDialog(new JFrame(), msg, "Usage", JOptionPane.INFORMATION_MESSAGE);
 		}
 		MapTool.main(args);
 	}
