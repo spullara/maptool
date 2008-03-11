f0b6916166d51d981d5ce8dc4e34af2c322124f3
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2008-03-11 01:25:35 +0000

    rebuild message panel image handling

---
+++ src/net/rptools/maptool/client/swing/MessagePanelEditorKit.java
@@ -0,0 +1,52 @@
+package net.rptools.maptool.client.swing;
+
+import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
+
+public class MessagePanelEditorKit extends HTMLEditorKit {
+
+	private ViewFactory viewFactory = new MessagePanelViewFactory();
+
+	private HTMLPanelImageCache imageCache = new HTMLPanelImageCache();
+
+	public MessagePanelEditorKit() {
+		viewFactory = new MessagePanelViewFactory();
+	}
+	
+	@Override
+	public ViewFactory getViewFactory() {
+		return viewFactory;
+	}
+	
+	public void flush() {
+		imageCache.flush();
+	}
+	
+	private class MessagePanelViewFactory extends HTMLFactory {
+
+		@Override
+		public View create(Element elem) {
+			
+	        AttributeSet attrs = elem.getAttributes();
+	 	    Object elementName = attrs.getAttribute(AbstractDocument.ElementNameAttribute);
+		    Object o = (elementName != null) ? null : attrs.getAttribute(StyleConstants.NameAttribute);
+		    if (o instanceof HTML.Tag) {
+				HTML.Tag kind = (HTML.Tag) o;
+				if (kind == HTML.Tag.IMG) {
+				    return new MessagePanelImageView(elem, imageCache);
+				}		
+		    }
+			return super.create(elem);
+		}
+	}
+}
