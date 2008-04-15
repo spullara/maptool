5c1920a29fc5fd563c783781557182f51a11e6c3
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2008-04-15 21:02:41 +0000

    tweak for DV & LL

--- src/net/rptools/maptool/model/CampaignProperties.java
+++ src/net/rptools/maptool/model/CampaignProperties.java
@@ -186,7 +186,7 @@ public class CampaignProperties {
     	
     	try {
     		sightTypeMap.put("Darkvision", new SightType("Darkvision", 1, LightSource.getDefaultLightSources().get("Generic").get(5)));
-    		sightTypeMap.put("Darkvision & Lowlight", new SightType("Darkvision", 2, LightSource.getDefaultLightSources().get("Generic").get(5)));
+    		sightTypeMap.put("Darkvision & Lowlight", new SightType("Darkvision", 2, LightSource.getDefaultLightSources().get("Generic").get(1)));
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
