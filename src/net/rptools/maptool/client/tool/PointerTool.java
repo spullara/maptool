cdbf531dd418bdde553ab76333d42fd61397f81d
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2008-06-21 07:19:53 +0000

    fix bug: click drag token deselects token

--- src/net/rptools/maptool/client/tool/PointerTool.java
+++ src/net/rptools/maptool/client/tool/PointerTool.java
@@ -452,10 +452,10 @@ public class PointerTool extends DefaultTool implements ZoneOverlay {
                     renderer.clearSelectedTokens();
 				}
 				
-				if (!renderer.getSelectedTokenSet().contains(token.getId())) {
-					renderer.selectToken(token.getId());
-				} else {
+				if (SwingUtil.isShiftDown(e) && renderer.getSelectedTokenSet().contains(token.getId())) {
 					renderer.deselectToken(token.getId());
+				} else {
+					renderer.selectToken(token.getId());
 				}
         
 		        // Dragging offset for currently selected token
