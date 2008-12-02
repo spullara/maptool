92926524b4b5b1a6dad0e842be711b9d73e9f8c5
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2008-12-02 14:57:44 +0000

    fix "break" bug

--- src/net/rptools/maptool/server/ServerMethodHandler.java
+++ src/net/rptools/maptool/server/ServerMethodHandler.java
@@ -104,8 +104,8 @@ public class ServerMethodHandler extends AbstractMethodHandler implements Server
             case heartbeat:				  heartbeat(context.getString(0));break;
             case updateCampaign:		  updateCampaign((CampaignProperties) context.get(0));break;
             case movePointer: 			  movePointer(context.getString(0), context.getInt(1), context.getInt(2));break;
-            case updateInitiative:        updateInitiative((InitiativeList)context.get(0), (Boolean)context.get(1));
-            case setUseVision:            setUseVision(context.getGUID(0), context.getBool(1));
+            case updateInitiative:        updateInitiative((InitiativeList)context.get(0), (Boolean)context.get(1));break;
+            case setUseVision:            setUseVision(context.getGUID(0), context.getBool(1));break;
             }
         } finally {
             RPCContext.setCurrent(null);
