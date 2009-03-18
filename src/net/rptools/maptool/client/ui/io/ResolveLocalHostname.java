cd6b84025a82cffa80527168c0e9db1b567ceb2d
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2009-03-18 05:30:10 +0000

    revert inet addy lookup (doesn't work well with virtual nats) (Contributed by azhrei)

--- src/net/rptools/maptool/client/ui/io/ResolveLocalHostname.java
+++ src/net/rptools/maptool/client/ui/io/ResolveLocalHostname.java
@@ -3,9 +3,9 @@
  */
 package net.rptools.maptool.client.ui.io;
 
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  * @author crash
@@ -14,11 +14,8 @@ import java.net.SocketException;
 public class ResolveLocalHostname {
 	private static final int RANDOM_PORT = 54321;
 
-	public static InetAddress getLocalHost(InetAddress intendedDestination) throws SocketException {
-		DatagramSocket sock = new DatagramSocket(RANDOM_PORT);
-		sock.connect(intendedDestination, RANDOM_PORT);
-		InetAddress addr = sock.getLocalAddress();
-		sock.close(); // Cleanup as many resources as possible. :)
-		return addr;
+	public static InetAddress getLocalHost(InetAddress intendedDestination) throws UnknownHostException, SocketException {
+		InetAddress inet = InetAddress.getLocalHost(); // This determines whether IP4 or IP6
+		return inet;
 	}
 }
