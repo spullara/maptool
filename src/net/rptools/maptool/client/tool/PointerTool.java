3ef6c7af7c4111eb3549848e66a7bbad184c9d74
tcroft <tcroft@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2009-07-28 05:46:36 +0000

    Fix bug: don't expose fog on token move if fog is turned off

--- src/net/rptools/maptool/client/tool/PointerTool.java
+++ src/net/rptools/maptool/client/tool/PointerTool.java
@@ -208,7 +208,7 @@ public class PointerTool extends DefaultTool implements ZoneOverlay {
 		dragOffsetX = 0;
 		dragOffsetY = 0;
 
-		if (AppPreferences.getAutoRevealVisionOnGMMovement() && (MapTool.getPlayer().isGM() || MapTool.getServerPolicy().getPlayersCanRevealVision())) {
+		if (renderer.getZone().hasFog() && AppPreferences.getAutoRevealVisionOnGMMovement() && (MapTool.getPlayer().isGM() || MapTool.getServerPolicy().getPlayersCanRevealVision())) {
 			Set<GUID> exposeSet = new HashSet<GUID>();
 			for (GUID tokenGUID : renderer.getSelectedTokenSet()) {
 				Token token = renderer.getZone().getToken(tokenGUID);
