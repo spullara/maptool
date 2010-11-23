5daf89ee0b2ff4c6956d0a3254ad6cbf17240981
azhrei_fje <azhrei_fje@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2010-11-23 05:24:40 +0000

    aku_chatAutosaveExternalizstion.txt

--- src/net/rptools/maptool/client/ChatAutoSave.java
+++ src/net/rptools/maptool/client/ChatAutoSave.java
@@ -11,6 +11,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import net.rptools.maptool.client.ui.commandpanel.CommandPanel;
+import net.rptools.maptool.language.I18N;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
@@ -28,7 +29,7 @@ public class ChatAutoSave {
 	private static String chatlog = null;
 
 	private ChatAutoSave() {
-		log.debug("Creating chat log autosave timer");
+		log.debug("Creating chat log autosave timer"); //$NON-NLS-1$
 		// Only way to set the delay is to call changeTimeout()
 		delay = 0;
 		countdown = new Timer();
@@ -39,37 +40,37 @@ public class ChatAutoSave {
 			@Override
 			public void run() {
 				if (log.isDebugEnabled())
-					log.debug("Chat log autosave countdown complete from " + timeout);
+					log.debug("Chat log autosave countdown complete from " + timeout); //$NON-NLS-1$
 				if (chatlog == null) {
 					String filename = AppPreferences.getChatFilenameFormat();
 					// FJE Ugly kludge to replace older default entry with newer default
 					// TODO This is going into 1.3.b77 so remove it in 3-4 builds
-					if ("chatlog.html".equals(filename)) {
+					if ("chatlog.html".equals(filename)) { //$NON-NLS-1$
 						AppPreferences.clearChatFilenameFormat();
 						filename = AppPreferences.getChatFilenameFormat();
 					}
 					chatlog = String.format(filename, new Date()).replace(':', '-');
 				}
-				File chatFile = new File(AppUtil.getAppHome("autosave").toString(), chatlog);
+				File chatFile = new File(AppUtil.getAppHome("autosave").toString(), chatlog); //$NON-NLS-1$
 				if (log.isInfoEnabled())
-					log.info("Saving log to '" + chatFile + "'");
+					log.info("Saving log to '" + chatFile + "'"); //$NON-NLS-1$ //$NON-NLS-2$
 
 				FileWriter writer = null;
 				CommandPanel chat = MapTool.getFrame().getCommandPanel();
 				String old = MapTool.getFrame().getStatusMessage();
 				try {
-					MapTool.getFrame().setStatusMessage("Autosaving chat log...");
+					MapTool.getFrame().setStatusMessage(I18N.getString("ChatAutoSave.status.chatAutosave")); //$NON-NLS-1$
 					writer = new FileWriter(chatFile);
 					writer.write(chat.getMessageHistory());
 					if (log.isInfoEnabled())
-						log.info("Log saved");
+						log.info("Log saved"); //$NON-NLS-1$
 				} catch (IOException e) {
 					// If this happens should we track it and turn off the autosave?  Perhaps
 					// after a certain number of consecutive failures?  Or maybe just lengthen
 					// the amount of time between attempts in that case?  At a minimum we
 					// should probably give the user a chance to turn it off as part of this
 					// message box that pops up...
-					MapTool.showWarning("msg.warn.failedAutoSavingMessageHistory", e);
+					MapTool.showWarning("msg.warn.failedAutoSavingMessageHistory", e); //$NON-NLS-1$
 				} finally {
 					IOUtils.closeQuietly(writer);
 					MapTool.getFrame().setStatusMessage(old);
@@ -99,7 +100,7 @@ public class ChatAutoSave {
 		if (delay > 0) {
 			stop();
 			task = createTimer(delay);
-			countdown.schedule(task, 5000, delay);	// Wait 5s, then save the log every 'delay' ms
+			countdown.schedule(task, 5000, delay); // Wait 5s, then save the log every 'delay' ms
 		}
 	}
 }
