352552e9ae4fd4880869d149ff882c741b425101
azhrei_fje <azhrei_fje@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2010-12-20 02:52:11 +0000

    Formatting after reviewing potential causes for NPE

--- src/net/rptools/maptool/client/ui/assetpanel/Directory.java
+++ src/net/rptools/maptool/client/ui/assetpanel/Directory.java
@@ -1,15 +1,12 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package net.rptools.maptool.client.ui.assetpanel;
 
@@ -26,128 +23,120 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Directory {
+	private static final FileFilter DIRECTORY_FILTER = new DirectoryFileFilter();
+	private final List<PropertyChangeListener> listenerList = new CopyOnWriteArrayList<PropertyChangeListener>();
+	private final File directory;
 
-    private static final FileFilter DIRECTORY_FILTER = new DirectoryFileFilter();
+	private Directory parent;
+	private List<Directory> subdirs;
+	private List<File> files;
+	private final FilenameFilter fileFilter;
 
-    private List<PropertyChangeListener> listenerList = new CopyOnWriteArrayList<PropertyChangeListener>();  
-    
-    private File directory;
+	public Directory(File directory) {
+		this(directory, null);
+	}
 
-    private Directory parent;
-    private List<Directory> subdirs;
-    private List<File> files;
+	public Directory(File directory, FilenameFilter fileFilter) {
+		if (directory.exists() && !directory.isDirectory()) {
+			throw new IllegalArgumentException(directory + " is not a directory");
+		}
+		this.directory = directory;
+		this.fileFilter = fileFilter;
+	}
 
-    private FilenameFilter fileFilter;
-    
-    public Directory(File directory) {
-        this (directory, null);
-    }
-    public Directory(File directory, FilenameFilter fileFilter) {
-        if (directory.exists() && !directory.isDirectory()) {
-            throw new IllegalArgumentException(directory + " is not a directory");
-        }
-        
-        this.directory = directory;
-        this.fileFilter = fileFilter;
-    }
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		if (!listenerList.contains(listener)) {
+			listenerList.add(listener);
+		}
+	}
 
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        if (!listenerList.contains(listener)) {
-            listenerList.add(listener);
-        }
-    }
-    
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        listenerList.remove(listener);
-    }
-    
-    public boolean equals(Object o) {
-        if (!(o instanceof Directory)) {
-            return false;
-        }
-        
-        return directory.equals(((Directory)o).directory);
-    }
-    
-    public File getPath() {
-        return directory;
-    }
-    
-    public void refresh() {
-    	subdirs = null;
-    	files = null;
-    }
-    
-    public List<Directory> getSubDirs() {
-        load();
-        return subdirs;
-    }
-    
-    public List<File> getFiles() {
-        load();
-        return files;
-    }
-    
-    public Directory getParent() {
-        return parent;
-    }
-    
-    private void load() {
-        if (files == null && subdirs == null) {
-        	if (!directory.exists()) {
-        		files = new ArrayList<File>();
-        		subdirs = new ArrayList<Directory>();
-        		return;
-        	}
-            files = Collections.unmodifiableList(Arrays.asList(directory.listFiles(fileFilter)));
-            File [] subdirList = directory.listFiles(DIRECTORY_FILTER);
-            subdirs = new ArrayList<Directory>();
-            for (int i = 0; i < subdirList.length; i++) {
-                Directory newDir = newDirectory(subdirList[i], fileFilter); 
-                newDir.parent = this;
-                subdirs.add(newDir);
-            }
-            Collections.sort(subdirs, new Comparator<Directory>() {
-            	public int compare(Directory d1, Directory d2) {
-            		String name1 = d1.getPath().getName();
-            		String name2 = d2.getPath().getName();
-            		
-            		return String.CASE_INSENSITIVE_ORDER.compare(name1, name2);
-            	}
-            });
-            subdirs = Collections.unmodifiableList(subdirs);
-        }
-    }
-    
-    private static class DirectoryFileFilter implements FileFilter {
-        
-        public boolean accept(File pathname) {
-            return pathname.isDirectory();
-        }
-    }
-    
-    protected Directory newDirectory(File directory, FilenameFilter fileFilter) {
-    	return new Directory(directory, fileFilter);
-    }
-    
-    protected void firePropertyChangeEvent(PropertyChangeEvent event) {
-        
-    	// Me
-        for (PropertyChangeListener listener : listenerList) {
-            listener.propertyChange(event);
-        }
-        
-        // Propagate up
-        if (parent != null) {
-        	parent.firePropertyChangeEvent(event);
-        }
-    }
-    
-    public static final Comparator<Directory> COMPARATOR = new Comparator<Directory>() {
-    	public int compare(Directory o1, Directory o2) {
-    		String filename1 = o1.getPath().getName();
-    		String filename2 = o2.getPath().getName();
-    		return filename1.compareToIgnoreCase(filename2);
-    	}
-    };
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		listenerList.remove(listener);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Directory)) {
+			return false;
+		}
+		return directory.equals(((Directory) o).directory);
+	}
+
+	public File getPath() {
+		return directory;
+	}
+
+	public void refresh() {
+		subdirs = null;
+		files = null;
+	}
+
+	public List<Directory> getSubDirs() {
+		load();
+		return subdirs;
+	}
+
+	public List<File> getFiles() {
+		load();
+		return files;
+	}
+
+	public Directory getParent() {
+		return parent;
+	}
+
+	private void load() {
+		if (files == null && subdirs == null) {
+			if (!directory.exists()) {
+				files = new ArrayList<File>();
+				subdirs = new ArrayList<Directory>();
+				return;
+			}
+			files = Collections.unmodifiableList(Arrays.asList(directory.listFiles(fileFilter)));
+			File[] subdirList = directory.listFiles(DIRECTORY_FILTER);
+			subdirs = new ArrayList<Directory>();
+			for (int i = 0; i < subdirList.length; i++) {
+				Directory newDir = newDirectory(subdirList[i], fileFilter);
+				newDir.parent = this;
+				subdirs.add(newDir);
+			}
+			Collections.sort(subdirs, new Comparator<Directory>() {
+				public int compare(Directory d1, Directory d2) {
+					String name1 = d1.getPath().getName();
+					String name2 = d2.getPath().getName();
+					return String.CASE_INSENSITIVE_ORDER.compare(name1, name2);
+				}
+			});
+			subdirs = Collections.unmodifiableList(subdirs);
+		}
+	}
+
+	private static class DirectoryFileFilter implements FileFilter {
+		public boolean accept(File pathname) {
+			return pathname.isDirectory();
+		}
+	}
+
+	protected Directory newDirectory(File directory, FilenameFilter fileFilter) {
+		return new Directory(directory, fileFilter);
+	}
+
+	protected void firePropertyChangeEvent(PropertyChangeEvent event) {
+		// Me
+		for (PropertyChangeListener listener : listenerList) {
+			listener.propertyChange(event);
+		}
+		// Propagate up
+		if (parent != null) {
+			parent.firePropertyChangeEvent(event);
+		}
+	}
+
+	public static final Comparator<Directory> COMPARATOR = new Comparator<Directory>() {
+		public int compare(Directory o1, Directory o2) {
+			String filename1 = o1.getPath().getName();
+			String filename2 = o2.getPath().getName();
+			return filename1.compareToIgnoreCase(filename2);
+		}
+	};
 }
--- src/net/rptools/maptool/client/ui/assetpanel/ImageFileTreeModel.java
+++ src/net/rptools/maptool/client/ui/assetpanel/ImageFileTreeModel.java
@@ -1,15 +1,12 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package net.rptools.maptool.client.ui.assetpanel;
 
@@ -22,144 +19,139 @@ import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
-import net.rptools.maptool.model.AssetGroup;
-
-
 /**
  */
 public class ImageFileTreeModel implements TreeModel {
+	private final List<Directory> rootDirectories = new ArrayList<Directory>();
+	private final Object root = new String("");
+	private final List<TreeModelListener> listenerList = new ArrayList<TreeModelListener>();
 
-    private List<Directory> rootDirectories = new ArrayList<Directory>();
+	public ImageFileTreeModel() {
+	}
 
-    private Object root = new String("");
-    
-    private List<TreeModelListener> listenerList = new ArrayList<TreeModelListener>();
-    
-    public ImageFileTreeModel() {
-    }
-    
-    public boolean isRootGroup(Directory dir) {
-        return rootDirectories.contains(dir);
-    }
-    
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#getRoot()
-     */
-    public Object getRoot() {
-        return root;
-    }
+	public boolean isRootGroup(Directory dir) {
+		return rootDirectories.contains(dir);
+	}
 
-    public boolean containsRootGroup(Directory dir) {
-    	
-    	for (Directory directory : rootDirectories) {
-    		if (directory.getPath().equals(dir.getPath())) {
-    			return true;
-    		}
-    	}
-    	
-    	return false;
-    }
-    
-    public void addRootGroup (Directory directory) {
-    	rootDirectories.add(directory);
-    	Collections.sort(rootDirectories, Directory.COMPARATOR);
-        fireStructureChangedEvent(new TreeModelEvent(this, new Object[]{getRoot()}, 
-          new int[] { rootDirectories.size() - 1 }, new Object[] {directory}));
-    }
-    
-    public void removeRootGroup (Directory directory) {
-        rootDirectories.remove(directory);
-        fireStructureChangedEvent(new TreeModelEvent(this, new Object[]{getRoot()}));
-    }
-    
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#getChild(java.lang.Object, int)
-     */
-    public Object getChild(Object parent, int index) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#getRoot()
+	 */
+	public Object getRoot() {
+		return root;
+	}
 
-    	if (parent == root) {
-    		return rootDirectories.get(index);
-    	}
-    	
-        Directory dir = (Directory) parent;
-        
-        return dir.getSubDirs().get(index);
-    }
+	public boolean containsRootGroup(Directory dir) {
+		for (Directory directory : rootDirectories) {
+			if (directory.getPath().equals(dir.getPath())) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#getChildCount(java.lang.Object)
-     */
-    public int getChildCount(Object parent) {
-        
-        if (parent == root) {
-            return rootDirectories.size();
-        }
-        
-        Directory dir = (Directory) parent;
-        
-        return dir.getSubDirs().size();
-    }
+	public void addRootGroup(Directory directory) {
+		rootDirectories.add(directory);
+		Collections.sort(rootDirectories, Directory.COMPARATOR);
+		fireStructureChangedEvent(new TreeModelEvent(this, new Object[] { getRoot() }, new int[] { rootDirectories.size() - 1 }, new Object[] { directory }));
+	}
 
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#isLeaf(java.lang.Object)
-     */
-    public boolean isLeaf(Object node) {
+	public void removeRootGroup(Directory directory) {
+		rootDirectories.remove(directory);
+		fireStructureChangedEvent(new TreeModelEvent(this, new Object[] { getRoot() }));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#getChild(java.lang.Object, int)
+	 */
+	public Object getChild(Object parent, int index) {
+		if (parent == root) {
+			return rootDirectories.get(index);
+		}
+		Directory dir = (Directory) parent;
+		return dir.getSubDirs().get(index);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#getChildCount(java.lang.Object)
+	 */
+	public int getChildCount(Object parent) {
+		if (parent == root) {
+			return rootDirectories.size();
+		}
+		Directory dir = (Directory) parent;
+		return dir.getSubDirs().size();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#isLeaf(java.lang.Object)
+	 */
+	public boolean isLeaf(Object node) {
 		// No leaves here
-        return false;
-    }
+		return false;
+	}
 
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#valueForPathChanged(javax.swing.tree.TreePath, java.lang.Object)
-     */
-    public void valueForPathChanged(TreePath path, Object newValue) {
-        // Nothing to do right now
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#valueForPathChanged(javax.swing.tree.TreePath, java.lang.Object)
+	 */
+	public void valueForPathChanged(TreePath path, Object newValue) {
+		// Nothing to do right now
+	}
 
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#getIndexOfChild(java.lang.Object, java.lang.Object)
-     */
-    public int getIndexOfChild(Object parent, Object child) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#getIndexOfChild(java.lang.Object, java.lang.Object)
+	 */
+	public int getIndexOfChild(Object parent, Object child) {
+		if (parent == root) {
+			return rootDirectories.indexOf(child);
+		}
+		Directory dir = (Directory) parent;
+		return dir.getSubDirs().indexOf(child);
+	}
 
-    	if (parent == root) {
-    		return rootDirectories.indexOf(child);
-    	}
-    	
-        Directory dir = (Directory) parent;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#addTreeModelListener(javax.swing.event.TreeModelListener)
+	 */
+	public void addTreeModelListener(TreeModelListener l) {
+		listenerList.add(l);
+	}
 
-        return dir.getSubDirs().indexOf(child);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.tree.TreeModel#removeTreeModelListener(javax.swing.event.TreeModelListener)
+	 */
+	public void removeTreeModelListener(TreeModelListener l) {
+		listenerList.remove(l);
+	}
 
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#addTreeModelListener(javax.swing.event.TreeModelListener)
-     */
-    public void addTreeModelListener(TreeModelListener l) {
-        listenerList.add(l);
-    }
+	public void refresh() {
+	}
 
-    /* (non-Javadoc)
-     * @see javax.swing.tree.TreeModel#removeTreeModelListener(javax.swing.event.TreeModelListener)
-     */
-    public void removeTreeModelListener(TreeModelListener l) {
-        listenerList.remove(l);
-    }
+	private void fireStructureChangedEvent(TreeModelEvent e) {
+		TreeModelListener[] listeners = listenerList.toArray(new TreeModelListener[listenerList.size()]);
+		for (TreeModelListener listener : listeners) {
+			listener.treeStructureChanged(e);
+		}
+	}
 
-    public void refresh() {
-    }
-
-    private void fireStructureChangedEvent(TreeModelEvent e) {
-      TreeModelListener[] listeners = listenerList.toArray(new TreeModelListener[listenerList.size()]);
-      for (TreeModelListener listener : listeners) {
-        listener.treeStructureChanged(e);
-      }
-    }
-    
-    private void fireNodesInsertedEvent(TreeModelEvent e) {
-      TreeModelListener[] listeners = listenerList.toArray(new TreeModelListener[listenerList.size()]);
-      for (TreeModelListener listener : listeners) {
-        listener.treeNodesInserted(e);
-      }
-    }
-
-    
-
+	private void fireNodesInsertedEvent(TreeModelEvent e) {
+		TreeModelListener[] listeners = listenerList.toArray(new TreeModelListener[listenerList.size()]);
+		for (TreeModelListener listener : listeners) {
+			listener.treeNodesInserted(e);
+		}
+	}
 }
