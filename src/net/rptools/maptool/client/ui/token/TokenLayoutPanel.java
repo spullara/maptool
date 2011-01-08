02b47f5f08e733d4a0c9ecee2afbd1e72a594610
azhrei_fje <azhrei_fje@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2011-01-08 22:57:01 +0000

    Add status bar info when adjusting Layout field on token editor dialog; add double-click to reset

--- src/net/rptools/maptool/client/ui/token/TokenLayoutPanel.java
+++ src/net/rptools/maptool/client/ui/token/TokenLayoutPanel.java
@@ -1,15 +1,12 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package net.rptools.maptool.client.ui.token;
 
@@ -29,6 +26,7 @@ import java.awt.geom.Area;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import net.rptools.lib.swing.SwingUtil;
 import net.rptools.maptool.client.AppStyle;
@@ -38,10 +36,9 @@ import net.rptools.maptool.model.Zone;
 import net.rptools.maptool.util.ImageManager;
 
 /**
- * Support class used by the token editor dialog on the "Properties" tab to allow
- * a token's image to be moved around within a one-cell grid area.  Scaling is
- * supported using the mousewheel and position is supported using left-drag.
- * We should add rotation ability using Shift-mousewheel as well.
+ * Support class used by the token editor dialog on the "Properties" tab to allow a token's image to be moved around
+ * within a one-cell grid area. Scaling is supported using the mousewheel and position is supported using left-drag. We
+ * should add rotation ability using Shift-mousewheel as well.
  * 
  * @author trevor
  */
@@ -53,7 +50,7 @@ public class TokenLayoutPanel extends JPanel {
 	public TokenLayoutPanel() {
 		addMouseWheelListener(new MouseWheelListener() {
 			public void mouseWheelMoved(MouseWheelEvent e) {
-				// Not for non snap to scale
+				// Not for snap-to-scale
 				if (!token.isSnapToScale()) {
 					return;
 				}
@@ -77,10 +74,35 @@ public class TokenLayoutPanel extends JPanel {
 			}
 		});
 		addMouseListener(new MouseAdapter() {
+			String old;
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				dragOffsetX = e.getX();
 				dragOffsetY = e.getY();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				old = MapTool.getFrame().getStatusMessage();
+				MapTool.getFrame().setStatusMessage("Mouse Wheel to zoom; double-LClick to reset position and zoom");
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (old != null)
+					MapTool.getFrame().setStatusMessage(old);
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isLeftMouseButton(e) && e.getClickCount() == 2) {
+					dragOffsetX = 0;
+					dragOffsetY = 0;
+					token.setAnchor(0, 0);
+					token.setSizeScale(1.0);
+					repaint();
+				}
 			}
 		});
 		addMouseMotionListener(new MouseMotionAdapter() {
@@ -91,9 +113,10 @@ public class TokenLayoutPanel extends JPanel {
 
 				Zone zone = MapTool.getFrame().getCurrentZoneRenderer().getZone();
 
-				int halfGridSize = zone.getGrid().getSize() / 2;
-				int maxXoff = Math.max(halfGridSize, token.getBounds(zone).width - zone.getGrid().getSize());
-				int maxYoff = Math.max(halfGridSize, token.getBounds(zone).height - zone.getGrid().getSize());
+				int gridSize = zone.getGrid().getSize();
+				int halfGridSize = gridSize / 2;
+				int maxXoff = Math.max(halfGridSize, token.getBounds(zone).width - gridSize);
+				int maxYoff = Math.max(halfGridSize, token.getBounds(zone).height - gridSize);
 
 				int offX = Math.min(maxXoff, Math.max(token.getAnchor().x + dx, -maxXoff));
 				int offY = Math.min(maxYoff, Math.max(token.getAnchor().y + dy, -maxYoff));
@@ -134,30 +157,30 @@ public class TokenLayoutPanel extends JPanel {
 		Dimension imgSize = new Dimension(image.getWidth(), image.getHeight());
 		SwingUtil.constrainTo(imgSize, tokenSize.width, tokenSize.height);
 
-		Point centerPoint = new Point(size.width/2, size.height/2);
-
+		Point centerPoint = new Point(size.width / 2, size.height / 2);
 		Graphics2D g2d = (Graphics2D) g;
 
 		// Background
-		((Graphics2D)g).setPaint(new TexturePaint(AppStyle.panelTexture, new Rectangle(0, 0, AppStyle.panelTexture.getWidth(), AppStyle.panelTexture.getHeight())));
+		((Graphics2D) g).setPaint(new TexturePaint(AppStyle.panelTexture, new Rectangle(0, 0, AppStyle.panelTexture.getWidth(), AppStyle.panelTexture.getHeight())));
 		g2d.fillRect(0, 0, size.width, size.height);
-		AppStyle.shadowBorder.paintWithin((Graphics2D)g, 0, 0, size.width, size.height);
+		AppStyle.shadowBorder.paintWithin((Graphics2D) g, 0, 0, size.width, size.height);
 
 		// Grid
 		if (zone.getGrid().getCapabilities().isSnapToGridSupported()) {
 			Area gridShape = zone.getGrid().getCellShape();
-			int offsetX = (size.width - gridShape.getBounds().width)/2;
-			int offsetY = (size.height - gridShape.getBounds().height)/2;
+			int offsetX = (size.width - gridShape.getBounds().width) / 2;
+			int offsetY = (size.height - gridShape.getBounds().height) / 2;
 			g2d.setColor(Color.black);
 
-			g2d.drawLine(0, size.height/2, size.width, size.height/2);
-			g2d.drawLine(size.width/2, 0,  size.width/2, size.height);
+			// Add horizontal and vertical lines to help with centering
+			g2d.drawLine(0, size.height / 2, size.width, size.height / 2);
+			g2d.drawLine(size.width / 2, 0, size.width / 2, size.height);
 
 			g2d.translate(offsetX, offsetY);
 			g2d.draw(gridShape);
 			g2d.translate(-offsetX, -offsetY);
 		}
 		// Token
-		g2d.drawImage(image, centerPoint.x - imgSize.width/2 + token.getAnchor().x, centerPoint.y - imgSize.height/2 + token.getAnchor().y, imgSize.width, imgSize.height, this);
+		g2d.drawImage(image, centerPoint.x - imgSize.width / 2 + token.getAnchor().x, centerPoint.y - imgSize.height / 2 + token.getAnchor().y, imgSize.width, imgSize.height, this);
 	}
 }
