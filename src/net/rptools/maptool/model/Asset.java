f19205f66b41ed6b78f77e65a3bfb45ed8b1ef1c
azhrei_fje <azhrei_fje@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2011-01-17 00:45:38 +0000

    Add debugging code for NPE; formatting

--- src/net/rptools/maptool/model/Asset.java
+++ src/net/rptools/maptool/model/Asset.java
@@ -1,9 +1,9 @@
 /*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -85,7 +85,7 @@ public class Asset {
 					}
 				}
 			} catch (IOException e) {
-				MapTool.showError("IOException?!", e);	// Can this happen??
+				MapTool.showError("IOException?!", e); // Can this happen??
 			}
 		}
 		return extension;
@@ -97,6 +97,11 @@ public class Asset {
 
 	public boolean isTransfering() {
 		return AssetManager.isAssetRequested(id);
+	}
+
+	@Override
+	public String toString() {
+		return id + "/" + name + "(" + (image != null ? image.length : "-") + ")";
 	}
 
 	@Override
