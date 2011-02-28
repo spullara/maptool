19a3579b2c9cc2567360a7b0380e442a84fec331
azhrei_fje <azhrei_fje@c4b8a87f-a10d-0410-b538-9c82ba3f8395>
2011-02-28 05:49:39 +0000

    Check global macros to ensure they don't overflow an OS-defined size limit

--- src/net/rptools/maptool/client/ui/macrobuttons/buttons/MacroButtonPrefs.java
+++ src/net/rptools/maptool/client/ui/macrobuttons/buttons/MacroButtonPrefs.java
@@ -1,15 +1,12 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package net.rptools.maptool.client.ui.macrobuttons.buttons;
 
@@ -24,12 +21,10 @@ import net.rptools.maptool.client.ui.MacroButtonHotKeyManager;
 import net.rptools.maptool.model.MacroButtonProperties;
 
 /**
- * Class that is responsible for storing global macro buttons' data in
- * the registry (on Windows that is, dunno where it stores on other platforms, duh.)
- * so it persists across sessions.
+ * Class that is responsible for storing global macro buttons' data in the registry (on Windows that is, dunno where it
+ * stores on other platforms, duh.) so it persists across sessions.
  */
 public class MacroButtonPrefs {
-
 	private static int maxIndex = 0;
 
 	private static final String PREF_COLOR_KEY = "color";
@@ -44,7 +39,7 @@ public class MacroButtonPrefs {
 	private static final String PREF_FONT_COLOR_KEY = "fontColorKey";
 	private static final String PREF_FONT_SIZE = "fontSize";
 	private static final String PREF_MIN_WIDTH = "minWidth";
-	private static final String PREF_MAX_WIDTH = "maxWidth";	
+	private static final String PREF_MAX_WIDTH = "maxWidth";
 	private static final String PREF_TOOLTIP = "toolTip";
 
 	private static final String FORMAT_STRING = "%010d";
@@ -54,24 +49,33 @@ public class MacroButtonPrefs {
 
 	public static void savePreferences(MacroButtonProperties properties) {
 		int index = properties.getIndex();
-		
+
 		// use zero padding to ensure proper ordering in the registry (otherwise 10 will come before 2 etc.)
-		String paddedIndex = String.format(FORMAT_STRING, index); 
-		
-		//prefs = Preferences.userRoot().node(AppConstants.APP_NAME + "/macros/" + paddedIndex);
+		String paddedIndex = String.format(FORMAT_STRING, index);
+
+//		prefs = Preferences.userRoot().node(AppConstants.APP_NAME + "/macros/" + paddedIndex);
 		Preferences prefs = Preferences.userRoot().node(AppConstants.APP_NAME + "/macros");
 
 		try {
 			if (!prefs.nodeExists(paddedIndex)) {
-					prefs = prefs.node(paddedIndex);
+				prefs = prefs.node(paddedIndex);
 			} else {
 				prefs = prefs.node(paddedIndex);
+			}
+			// Start with the macro text itself.  Apparently Windows has a length limit for registry values and the JRE doesn't
+			// re-read the registry to determine if all information was actually written...  So we'll do it ourselves.  We start with
+			// the macro text because if it fails, we don't want to bother saving the rest of the fields.
+			String text = properties.getCommand();
+			prefs.put(PREF_COMMAND_KEY, text);
+			String readback = prefs.get(PREF_COMMAND_KEY, null);
+			if (readback == null || !readback.equals(text)) {
+				MapTool.showError("Macro body not written properly?!  Operating system may have truncated to " + Preferences.MAX_VALUE_LENGTH + " characters.");
+				return;
 			}
 			prefs.put(PREF_COLOR_KEY, properties.getColorKey());
 			prefs.put(PREF_LABEL_KEY, properties.getLabel());
 			prefs.put(PREF_GROUP_KEY, properties.getGroup());
 			prefs.put(PREF_SORTBY_KEY, properties.getSortby());
-			prefs.put(PREF_COMMAND_KEY, properties.getCommand());
 			prefs.putBoolean(PREF_AUTO_EXECUTE, properties.getAutoExecute());
 			prefs.putBoolean(PREF_INCLUDE_LABEL, properties.getIncludeLabel());
 			prefs.putBoolean(PREF_APPLYTOTOKENS, properties.getApplyToTokens());
@@ -81,17 +85,17 @@ public class MacroButtonPrefs {
 			prefs.put(PREF_MIN_WIDTH, properties.getMinWidth());
 			prefs.put(PREF_MAX_WIDTH, properties.getMaxWidth());
 			prefs.put(PREF_TOOLTIP, properties.getToolTip());
+			prefs.flush();
 			MapTool.getFrame().getGlobalPanel().reset();
 		} catch (BackingStoreException e) {
-			e.printStackTrace();
+			MapTool.showError("Problem saving Global macros?!", e);
 		}
 	}
 
-	public static List<MacroButtonProperties> getButtonProperties()
-	{
+	public static List<MacroButtonProperties> getButtonProperties() {
 		List<MacroButtonProperties> buttonProperties = new ArrayList<MacroButtonProperties>();
 		Preferences prefsRoot = Preferences.userRoot().node(AppConstants.APP_NAME + "/macros");
-		
+
 		try {
 			for (String buttonNode : prefsRoot.childrenNames()) {
 				Preferences buttonPref = prefsRoot.node(buttonNode);
@@ -105,7 +109,6 @@ public class MacroButtonPrefs {
 				if (index > maxIndex) {
 					maxIndex = index;
 				}
-				
 				if (buttonNode.length() <= 3) {
 					// we have an old style button pref. copy it to the new format
 					// but check whether we have a new style pref first
@@ -144,15 +147,14 @@ public class MacroButtonPrefs {
 							String maxWidth = buttonPref.get(PREF_MAX_WIDTH, "");
 							String toolTip = buttonPref.get(PREF_TOOLTIP, "");
 
-							buttonProperties.add(new MacroButtonProperties(index, colorKey, hotKey, command, label, group, sortby, autoExecute, includeLabel, applyToTokens, fontColorKey, fontSize, minWidth, maxWidth, toolTip));
+							buttonProperties.add(new MacroButtonProperties(index, colorKey, hotKey, command, label, group, sortby, autoExecute, includeLabel, applyToTokens, fontColorKey, fontSize,
+									minWidth, maxWidth, toolTip));
 						}
 					}
-					
 					// set old pref to be removed (regardless it was copied or not)
 					buttonPref.removeNode();
 					continue;
 				}
-
 				String colorKey = buttonPref.get(PREF_COLOR_KEY, "");
 				String label = buttonPref.get(PREF_LABEL_KEY, Integer.toString(index));
 				String group = buttonPref.get(PREF_GROUP_KEY, "");
@@ -168,20 +170,20 @@ public class MacroButtonPrefs {
 				String hotKey = buttonPref.get(PREF_HOTKEY_KEY, MacroButtonHotKeyManager.HOTKEYS[0]);
 				String toolTip = buttonPref.get(PREF_TOOLTIP, "");
 
-				buttonProperties.add(new MacroButtonProperties(index, colorKey, hotKey, command, label, group, sortby, autoExecute, includeLabel, applyToTokens, fontColorKey, fontSize, minWidth, maxWidth, toolTip));
+				buttonProperties.add(new MacroButtonProperties(index, colorKey, hotKey, command, label, group, sortby, autoExecute, includeLabel, applyToTokens, fontColorKey, fontSize, minWidth,
+						maxWidth, toolTip));
 			}
 		} catch (BackingStoreException e) {
 			// exception due to prefsRoot.childrenNames()
-			e.printStackTrace();
+			MapTool.showError("Problem iterating over all Global macros?!", e);
 		}
-		
 		return buttonProperties;
 	}
-	
+
 	public static int getNextIndex() {
 		return ++maxIndex;
 	}
-	
+
 	public static void delete(MacroButtonProperties properties) {
 		int index = properties.getIndex();
 		String paddedIndex = String.format(FORMAT_STRING, index);
@@ -189,16 +191,17 @@ public class MacroButtonPrefs {
 		try {
 			prefs.removeNode();
 		} catch (BackingStoreException e) {
-			e.printStackTrace();
+			MapTool.showError("Problem when removing a Global macro?!", e);
 		}
 		MapTool.getFrame().getGlobalPanel().reset();
 	}
+
 	public static void deletePanel() {
 		Preferences prefs = Preferences.userRoot().node(AppConstants.APP_NAME + "/macros");
 		try {
 			prefs.removeNode();
 		} catch (BackingStoreException e) {
-			e.printStackTrace();
+			MapTool.showError("Problem when removing a Global macro?!", e);
 		}
 	}
 }
